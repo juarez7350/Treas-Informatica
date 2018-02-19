933d5c3633f6b29c4343f74d0602a421809ba707
David Juarez <daviddjuarez98@gmail.com>
2018-02-18 21:37:38 -0600

    hoja de trabajo 3

---
+++ TAREA/TAREA/ArrayList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA
+{
+    class ArrayList
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TAREA/TAREA/IList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA
+{
+    public interface IList<T>
+    {
+        bool Get(int indice);
+        bool Set(int indice, T elemento);
+        void Push(T elemento);
+        int Lenght { get; }
+    }
+}
