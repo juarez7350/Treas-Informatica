0101ab4c3736a299d97eb9056ddad04d2ed52192
David Juarez <daviddjuarez98@gmail.com>
2018-03-01 22:23:23 -0600

    hoja 5

---
+++ Hoja5/Hoja5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoja5
+{
+    public class Program
+    {
+        public static int ReduceInt(int [] valores, int accumulador, Func<int,int,int> reduccion)
+        {
+            for (int i = 0; i < valores.Length; i++)
+            {
+                accumulador = reduccion(accumulador, valores[i]);
+            }
+
+            return accumulador;
+        }
+
+        public static A Reduce<T, A>(T[] valores, A accumulador, Func<A, T, A> reduccion)
+        {
+            for (int i = 0; i < valores.Length; i++)
+            {
+                accumulador = reduccion(accumulador, valores[i]);
+            }
+
+            return accumulador;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Hoja5/ReduceTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hoja5;
+
+namespace ReduceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestReduceNormal()
+        {
+            int[] valores = new int[] { 2, 4, 5, 9 };
+            Assert.AreEqual(Program.ReduceInt(valores, 0, (acumulador, valor) => acumulador + valor), 20);
+        }
+
+        [TestMethod]
+        public void TestReduceGeneric()
+        {
+            string[] valores = new string[] { "hola", "como", "estas" };
+            Assert.AreEqual(Program.Reduce<string, int>(valores, 0, (acumulador, valor) => acumulador + valor.Length), 13);
+        }
+    }
+}
